44cbb2734324180e75eb4407286f1ca4795fba2b
Mateus Gabriel da Silva <mateus.gabriel@vocedm.com.br>
2023-01-06 15:44:34 -0300

    Ajustando a estrutura do app

---
+++ src/telas/Cesta/components/Detalhes.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Image, StyleSheet, View } from 'react-native'
+import Texto from '../../../components/Texto'
+import logo from '../../../../assets/logo.png'
+
+export default function Detalhes() {
+    return <>
+        <Texto style={estilos.nome}>Cesta de verduras</Texto>
+        <View style={estilos.fazenda}>
+                <Image source={logo} style={estilos.imagemFazenda}/>
+        <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
+        </View>
+        <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Texto>
+        <Texto style={estilos.preco}>R$ 40,00</Texto>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    nome: {
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold'
+    },
+    fazenda : {
+        flexDirection: 'row',
+        paddingVertical: 12
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32,
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+    },
+    descricao: {
+        color: '#a3a3a3', 
+        fontSize: 16,
+        lineHeight: 26,
+    }, 
+    preco: {
+        color: '#2a9f85',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8
+    }
+})
